40cc453fdfa3c682894d13b2b73e47786fb9e13d
limsammy1@gmail.com <limsammy1@gmail.com>
2018-03-21 07:06:41 -0600

    WIP. Bootstrap not working right now. Need to integrate node lookup on button press

--- spec/features/visitor_visits_home_page_spec.rb
+++ spec/features/visitor_visits_home_page_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 describe "Visitor navigates to index" do
   context "Visitor sees correct elements" do
-    scenario "Visitor sees navbar" do
+    scenario "Visitor sees root index" do
       visit root_path
 
-      expect(current_path).to eq('/home')
-      expect(page).to have_content('Search for YouTube playlist:')
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('Search for YouTube Playlist:')
+    end
+
+    scenario "Visitor inputs a playlist and presses enter" do
+      visit root_path
+
+      expect(page).to have_content('Search for YouTube Playlist:')
+      within(:css, ".playlist_ID") do
+        fill_in 'form-control', with: 'PLJKDxKDJBeYnen4wYXDY8jhlcZsDAsaT6'
+      end
     end
   end
 end
